76f773aed0e2816fbac59b6ff558b51436aa0ce5
Ivan tweheyo <tweheyoivan@gmail.com>
2023-05-25 17:37:15 +0300

    added home slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './Components/Header';
+import HomeSlider from './Components/HomeSlider';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar/>
+      <HomeSlider />
     </div>
   );
 }
